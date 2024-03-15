59b930b8ba006954b9ccaf52a5a866ea0418c46c
Kinjal Thaker <kthaker@cybots.one>
2024-03-15 21:56:54 +0530

    reactivity.

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -4,7 +4,17 @@
     let name = 'Svelte';
     let src = '/image.gif';
     let alt = 'Svelte is loading';
-    let string = 'This string contains some <strong>HTML</strong>.'
+    let string = 'This string contains some <strong>HTML</strong>.';
+    let counter = 0;
+
+    function increament()
+    {
+        counter += 1;
+        if (counter > 10)
+        {
+            counter = 0;
+        }
+    }
 </script>
 
 <style>
@@ -13,6 +23,15 @@
         font-family: 'Comic Sans MS', cursive;
         font-size: 2em;
     }
+    button {
+        padding: 0.5rem 1rem;
+        background-color: blue;
+        color: white;
+        font-weight: bold;
+        border: none;
+        border-radius: 3px;
+        cursor: pointer;       
+    }
 </style>
 
 <h1>Welcome {name.toUpperCase()}!</h1>
@@ -20,3 +39,4 @@
 <p>Trying styling paragraph.</p>
 <Nested />
 <p>{@html string}</p>
+<button on:click={increament}>Click me {counter}</button>
