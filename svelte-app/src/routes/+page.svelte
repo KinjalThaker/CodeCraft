1be29f487cf9a045a831f6388febb51687ce1163
Kinjal Thaker <kthaker@cybots.one>
2024-03-15 21:21:52 +0530

    add styling.

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -4,5 +4,14 @@
     let alt = 'Svelte is loading';
 </script>
 
+<style>
+    p {
+        color:goldenrod;
+        font-family: 'Comic Sans MS', cursive;
+        font-size: 2em;
+    }
+</style>
+
 <h1>Welcome {name.toUpperCase()}!</h1>
 <img {src} {alt} />
+<p>Lorem Ipsum. </p>
