af7e0c4eacb028e24b43fd88ea750c7dbcc8b325
Kinjal Thaker <kthaker@cybots.one>
2024-08-01 20:38:16 +0530

    svelte practice 1

---
+++ my-svelte-app/src/App.svelte
@@ -0,0 +1,89 @@
+<script>
+	import Modal from './Modal.svelte';
+
+	let firstName = "Kinjal";
+	let lastName = "Thaker";
+	let showModal = false;
+
+	//reactive value - it will react on the value on which it is depend on.
+	$: fullName = `${firstName} ${lastName}`; //template string
+
+	//reactive statement - in which it will react line of code instead of just a value.
+	$: {
+		console.log(fullName);
+		console.log(beltColor);
+	}
+
+	let beltColor = "black";
+
+	//Arrow function
+	const changeColor = () => {
+		beltColor = "orange";
+	}
+
+	let people = [
+		{name: 'Kinjal', age: 10, id: 1},
+		{name: 'Jini', age: 20, id: 2},
+		{name: 'Mini', age: 30, id: 3},
+	];
+
+	const handleDelete = (id) => {
+		people = people.filter((people) => people.id != id)
+	}
+
+	const toggleModal = () => {
+		showModal = !showModal;
+	}
+</script>
+
+<Modal message="Sign up for offers." isPromo={false} {showModal} on:click={toggleModal} />
+
+<main>
+	<div style="border:solid 1px lightgray;" class="p-2">
+		<div>This block contains data binding, reactive value, reactive statement and method handling.</div>
+		<h1>Hello {fullName}!</h1>
+		<p style="color: {beltColor};">This is the {beltColor} belt.</p>
+		<button on:click={changeColor}>Change color</button>
+		<input type="text" bind:value={fullName}>
+	</div>
+	<div style="border:solid 1px lightgray; padding: 20px">
+		{#each people as person}
+			<div style="text-align: left; padding: 10px 0; float: left;">
+				<div>Name: {person.name}</div>
+				<div>Age: {person.age}</div>
+			</div>
+			<div>
+				<button on:click={() => handleDelete(person.id)}>Delete</button> <!-- inline event handling -->
+			</div>
+			<div style="clear: both;"></div>
+			<div style="border-bottom: solid 1px"></div>
+			{:else}
+			<div>There is no records found.</div>
+		{/each}
+	</div>
+	<div>
+		<button on:click|once={toggleModal}>Open Modal</button>
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ my-svelte-app/src/Modal.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let showModal = false;
+    export let isPromo = false;
+    export let message = '';
+</script>
+
+{#if showModal}
+    <div class="backdrop" class:promo={isPromo} on:click|self> <!-- Event forwarding -->
+        <div class="modal">
+            <div>{message}</div>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .backdrop {
+        background: rgba(0, 0, 0, 0.8);
+        width: 100%;
+        height: 100%;
+        position: fixed;
+    }
+
+    .modal {
+        padding: 10px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+        background: white;
+    }
+
+    .promo .modal {
+        background: crimson;
+    }
+</style>
